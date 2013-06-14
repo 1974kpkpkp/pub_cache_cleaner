77a16dc721bb6dee238472ad807423d749ef5e22
andrew <andrew.mezoni@gmail.com>
2013-06-14 18:54:12 +0600

    commit

---
+++ lib/dummy.dart
@@ -0,0 +1,6 @@
+/*
+--- 14.06.2013 18:52:22 Running pub publish ... ---
+Missing requirements:
+* You must have a non-empty "lib" directory.
+  Without that, users cannot import any code from your package. *
+*/
